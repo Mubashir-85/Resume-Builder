dc255d8a153db870e2f0b296ecaf656fc81e68aa
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-04-17 18:53:30 +0530

    feat: add Header component to App

---
+++ Resume-Builder/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='h-15 bg-slate-900 flex items-center justify-center'>
+        <h1 className='flex mx-auto items-center font-bold text-2xl transform text-white'>Resume Builder</h1>
+    </div>
+  )
+}
+
+export default Header
