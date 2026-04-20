2a3f1f550f7307f64449ae8f29aeb6ce45b17154
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-04-20 23:52:19 +0530

    feat: add drag-and-drop functionality to DragDrop component

--- Resume-Builder/src/Components/Home/DragDrop.jsx
+++ Resume-Builder/src/Components/Home/DragDrop.jsx
@@ -4,13 +4,31 @@ import { FaFileUpload } from "react-icons/fa";
 function DragDrop() {
   const uploadRef = useRef(null);
   const [file, setFile] = useState(null);
+  const [isDragOver, setIsDragOver] = useState(false);
   const handleChange = () => {
     console.log(uploadRef.current.files[0]);
     setFile(uploadRef.current.files[0]);
   };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    console.log("drag over");
+    setIsDragOver(true);
+  };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    console.log("file dropped");
+    console.log(e.dataTransfer.files[0]);
+    setFile(e.dataTransfer.files[0]);
+    setIsDragOver(false);
+  };
+
   return (
     <div className="bg-white h-95 rounded-2xl w-[80%] sm:w-[70%] md:w-[40%] lg:w-[45%] mx-auto mt-10">
-      <div className="flex flex-col items-center justify-center h-full">
+      <div className={`flex flex-col items-center justify-center h-full opacity-100 ${isDragOver ? "border-4 border-dashed border-blue-900" : ""}`}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      onDragLeave={()=>setIsDragOver(false)}
+      >
         <div className="bg-[#FAF8FF] h-15 w-15 rounded-full flex items-center justify-center">
           <FaFileUpload className="text-4xl text-blue-900" />
         </div>
